627cbe2c844c50b2a9d3eb7295fa08530cf72f78
GeistInDerSH <geistindersh@gmail.com>
2022-07-30 22:26:29 -0700

    Moved rand_list to it's own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
-use rand::Rng;
 
+mod alloc;
 mod math;
 
 #[pyfunction]
@@ -9,23 +9,12 @@ fn int_sum(vec: Vec<usize>) -> PyResult<usize> {
     Ok(vec.iter().sum())
 }
 
-#[pyfunction]
-/// Generate a list of n random numbers with each number bounded by some number
-fn rand_list(len: usize, bound: usize) -> PyResult<Vec<usize>> {
-    let mut rng = rand::thread_rng();
-    let v = (0..len)
-        .map(|_| {
-            let n: usize = rng.gen();
-            n % bound
-        })
-        .collect::<Vec<usize>>();
-    Ok(v)
-}
-
 #[pymodule]
 fn rpython(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(int_sum, m)?)?;
     m.add_function(wrap_pyfunction!(math::square_and_multiply, m)?)?;
-    m.add_function(wrap_pyfunction!(rand_list, m)?)?;
+    m.add_function(wrap_pyfunction!(alloc::rand_list, m)?)?;
+    m.add_function(wrap_pyfunction!(alloc::serial_rand_list, m)?)?;
+    m.add_function(wrap_pyfunction!(alloc::parallel_rand_list, m)?)?;
     Ok(())
 }
