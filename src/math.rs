8a5d0f3a8eee69c5ce6370bf8f73c2d5eb9f3523
GeistInDerSH <geistindersh@gmail.com>
2022-08-01 19:17:24 -0700

    Added additional functions
    Added shortest_accending_subsequence function
    Added fib function

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,8 @@
 use pyo3::prelude::*;
 
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+
 /// Count the number of bits in an integer
 fn bit_count(n: u64) -> i64 {
     let mut count = 0;
@@ -26,4 +29,30 @@ pub fn square_and_multiply(base: u64, pow: u64, m: u64) -> PyResult<u64> {
     }
 
     Ok(n)
+}
+
+#[pyfunction]
+pub fn shortest_accending_subsequence(seq: Vec<usize>) -> PyResult<Vec<usize>> {
+    let mut sub_seq = Vec::new();
+
+    for val in seq {
+        if sub_seq.is_empty() || sub_seq[sub_seq.len() - 1] < val {
+            sub_seq.push(val);
+        }
+    }
+
+    Ok(sub_seq)
+}
+
+#[pyfunction]
+pub fn fib(n: usize) -> PyResult<BigUint> {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        f0 = std::mem::replace(&mut f1, f2);
+    }
+
+    Ok(f0)
 }
