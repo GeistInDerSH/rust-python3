21b4f6cf57970e161b7e2f2173c421a2a292fd46
GeistInDerSH <geistindersh@gmail.com>
2023-04-15 21:53:47 -0700

    Add grep_files_mapped function
    Sometimes we need to grep files, but know what files the strings were found in. As opposed to constructing the map ourselves, let Rust do that :)

--- src/grep.rs
+++ src/grep.rs
@@ -32,9 +32,25 @@ pub fn grep_files(regex: String, files: Vec<String>) -> PyResult<Vec<String>> {
         .collect())
 }
 
+#[pyfunction]
+pub fn grep_files_mapped(
+    regex: String,
+    files: Vec<String>,
+) -> PyResult<HashMap<String, Vec<String>>> {
+    Ok(files
+        .into_iter()
+        .collect::<HashSet<String>>() // Get the unique values
+        .iter()
+        .map(|file| (file.clone(), grep(regex.clone(), file.to_string())))
+        .filter(|(_, r)| r.is_ok())
+        .map(|(v, r)| (v, r.unwrap()))
+        .collect::<HashMap<String, Vec<String>>>())
+}
+
 pub fn register(py: Python<'_>) -> PyResult<&PyModule> {
     let m = PyModule::new(py, "grep")?;
     m.add_function(wrap_pyfunction!(grep_files, m)?)?;
+    m.add_function(wrap_pyfunction!(grep_files_mapped, m)?)?;
     m.add_function(wrap_pyfunction!(grep, m)?)?;
     Ok(m)
 }
