b9606b54b0c159c933b57d8451d7d0446b3531a6
GeistInDerSH <geistindersh@gmail.com>
2023-04-15 21:02:36 -0700

    Better error handling
    Do not blindly assume that the regex is correct

--- src/grep.rs
+++ src/grep.rs
@@ -1,13 +1,16 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyUnicodeError, prelude::*};
 use regex::Regex;
 
 #[pyfunction]
 pub fn grep(regex: String, file_name: String) -> PyResult<Vec<String>> {
     let file = File::open(file_name)?;
-    let regex = Regex::new(&regex).unwrap();
+    let regex = match Regex::new(&regex) {
+        Ok(r) => Ok(r),
+        Err(err) => Err(PyUnicodeError::new_err(err.to_string())),
+    }?;
 
     Ok(BufReader::new(file)
         .lines()
