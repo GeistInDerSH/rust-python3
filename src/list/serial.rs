5672e4e4d2b03a24e62787bcac09ed3b43c1375b
GeistInDerSH <geistindersh@gmail.com>
2023-04-15 21:09:45 -0700

    Simplify Code
    Simplify the code by using an iter, map, and collect

--- src/list/serial.rs
+++ src/list/serial.rs
@@ -4,23 +4,11 @@ use rand::Rng;
 #[pyfunction(bound = 256)]
 pub fn list_bounded(len: usize, bound: usize) -> PyResult<Vec<usize>> {
     let mut rng = rand::thread_rng();
-
-    let mut v = vec![0; len];
-    for item in v.iter_mut().take(len) {
-        *item = rng.gen_range(0..bound);
-    }
-
-    Ok(v)
+    Ok((0..len).map(|_| rng.gen_range(0..bound)).collect())
 }
 
 #[pyfunction]
 pub fn list(len: usize) -> PyResult<Vec<usize>> {
     let mut rng = rand::thread_rng();
-
-    let mut v = vec![0; len];
-    for item in v.iter_mut().take(len) {
-        *item = rng.gen();
-    }
-
-    Ok(v)
+    Ok((0..len).map(|_| rng.gen()).collect())
 }
