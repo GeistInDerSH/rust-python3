29eddf22baa280e73258fc45a18bdaa5f89490a4
GeistInDerSH <geistindersh@gmail.com>
2023-04-25 22:14:47 -0700

    Avoid mutability and join threads
    Avoid the need to declare the workers vector as mutable by making it a map call.
    
    Before we start making calls to receive the work, try to join all the threads to ensure they have finished

--- src/list/parallel.rs
+++ src/list/parallel.rs
@@ -14,33 +14,38 @@ pub fn list_bounded(len: usize, bound: usize) -> PyResult<Vec<usize>> {
 
     let (sender, receiver) = mpsc::channel();
     let sender = Arc::new(Mutex::new(sender));
-    let mut workers = Vec::with_capacity(core_count);
-    for _ in 0..core_count {
-        let sender = Arc::clone(&sender);
+    let workers = (0..core_count)
+        .map(|_| {
+            let sender = Arc::clone(&sender);
 
-        workers.push(thread::spawn(move || {
-            let mut rng = rand::thread_rng();
+            thread::spawn(move || {
+                let mut rng = rand::thread_rng();
 
-            let mut lv = vec![0; cap];
-            for item in lv.iter_mut().take(cap) {
-                *item = rng.gen_range(0..bound);
-            }
+                let mut lv = vec![0; cap];
+                for item in lv.iter_mut().take(cap) {
+                    *item = rng.gen_range(0..bound);
+                }
 
-            match sender.lock() {
-                Ok(mtx) => match mtx.send(lv) {
-                    Ok(_) => Ok(()),
+                match sender.lock() {
+                    Ok(mtx) => match mtx.send(lv) {
+                        Ok(_) => Ok(()),
+                        Err(err) => Err(PyException::new_err(err.to_string())),
+                    },
                     Err(err) => Err(PyException::new_err(err.to_string())),
-                },
-                Err(err) => Err(PyException::new_err(err.to_string())),
-            }
-        }));
-    }
+                }
+            })
+        })
+        .collect::<Vec<_>>();
 
     let mut v = Vec::with_capacity(len);
     let mut rng = rand::thread_rng();
     let remain = len % core_count;
     for _ in 0..remain {
         v.push(rng.gen_range(0..bound));
+    }
+
+    for thread in workers {
+        thread.join().expect("Failed to join thread")?;
     }
 
     for _ in 0..core_count {
